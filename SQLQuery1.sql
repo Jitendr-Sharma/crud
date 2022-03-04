9daf8f065fa32ac035670fcae72ebb0916d5b476
Jitendr-Sharma <100091356+Jitendr-Sharma@users.noreply.github.com>
2022-03-04 11:04:12 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,4 @@
+﻿select*from Employees
+DELETE FROM Employees WHERE id=5
+
+insert into Employees values ('hney','hk@','mbd',677776,'rj')
---
+++ SQLQuery2.sql
@@ -0,0 +1,4 @@
+﻿SELECT* FROM Employees
+SELECT*FROM Employees DELETE  ID = '7'
+
+DELETE FROM Employees WHERE ID='7';
